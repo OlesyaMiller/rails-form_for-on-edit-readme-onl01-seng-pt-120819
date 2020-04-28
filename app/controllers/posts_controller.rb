3055f8c8d2be4b7394c263eb6364af6442fcd557
github username <github email address>
2020-04-28 12:49:19 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 		@post.title = params[:title]
 		@post.description = params[:description]
 		@post.save
-		redirect_to post_path(@post)
+		redirect_to @post
 	end
 
 	def edit
@@ -26,6 +26,6 @@ class PostsController < ApplicationController
 	def update
 		@post = Post.find(params[:id])
 		@post.update(params.require(:post).permit(:title, :description))
-		redirect_to post_path(@post)
+		redirect_to @post
 	end
 end
